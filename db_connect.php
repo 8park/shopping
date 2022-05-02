847391a6b55282069d34afabc2178f70a4b6a821
AaronOrtiz01 <aarontheortiz@gmail.com>
2022-05-02 17:43:53 -0500

    fix ip

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-    $success = mysqli_connect('localhost', 'shopping6333', '123456789', 'shopping6333');
+    $success = mysqli_connect('https://west1-phpmyadmin.dreamhost.com/', 'shopping6333', '123456789', 'shopping6333');
 
     if(!$success){
         echo 'Connection Error: ' . mysqli_connect_error(); 
