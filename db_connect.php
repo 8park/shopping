ece64d8aff1c4eaaa92e0d4fb14e1b26018a825f
AaronOrtiz01 <aarontheortiz@gmail.com>
2022-05-02 18:29:07 -0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-    $success = mysqli_connect('https://west1-phpmyadmin.dreamhost.com/', 'shopping6333', '123456789', 'shopping6333');
+    $success = mysqli_connect('brighton.dreamhost.com', 'dh_t3hbtm', 'a6333a6333', 'shopping6333');
 
     if(!$success){
         echo 'Connection Error: ' . mysqli_connect_error(); 
