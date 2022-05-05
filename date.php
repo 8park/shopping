7e08192bcc4653fe366696d0590f8a5f6aa209b5
AaronOrtiz01 <aarontheortiz@gmail.com>
2022-05-05 16:35:13 -0500

    date

---
+++ date.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Stylesheet.css">
+    <title>Check by date</title>
+</head>
+<body>
+    <div class="signup_form">
+        <form method="post" action="confirm_login.php">
+        <h1 class="title">Login</h1>
+
+        <div class="inputContainer">
+        <h1>Show a Date Control</h1>
+
+    <form action="check_date.php">
+        <label for="date">Check date:</label>
+        <input type="date" id="date" name="date">
+        <input type="submit">
+    </form>
+        </div>
+
+            <p>
+                Need an account? <a href="check_date.php">Sign up</a></br>
+            </p>
+
+        </form>
+    </div>
+
+</br>
+</body>
+</html>
