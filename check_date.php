55f6ad6f6194b0a423bea220067ca5417431bf50
AaronOrtiz01 <aarontheortiz@gmail.com>
2022-05-05 17:11:40 -0500

    date

--- check_date.php
+++ check_date.php
@@ -1,35 +1,19 @@
     <?php
 
-    require_once 'db_connect.php';
+    require_once("db_connect.php");
+    require_once("db.php");
+    require_once("workerslist.php");
 
-
-    $email =  $_POST["email"];
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $password2 = $_POST["password_2"];
-    $address = $_POST["user_address"];
+    $date =  $_POST["date"];
     $flag = 0;
  
 
-    if (empty($username)){
-        echo '<script>alert("Username is required")</script>';
-        $flag = $flag + 1;
-    }
-    if (empty($email)) {
-        echo '<script>alert("Email is required")</script>';
-        $flag = $flag + 1;
-    }
-    if (empty($password)) {
-        echo '<script>alert("Password is required")</script>';
-        $flag = $flag + 1;
-    }
-    if ($password != $password2) {
-        echo '<script>alert("passwords do not match")</script>';
+    if (empty($date)){
+        echo '<script>alert("Error has occured, please try again")</script>';
         $flag = $flag + 1;
     }
 
-    
-    // first check the database for users that already exist with the same username or email
+/*
     $user_check_query = "SELECT * FROM customertb WHERE username='$username' OR email='$email' LIMIT 1";
     $result = mysqli_query($success, $user_check_query);
     $user = mysqli_fetch_assoc($result);
@@ -38,31 +22,23 @@
         echo '<script>alert("Username is taken")</script>';
         $flag = $flag + 1;
       }
-    
-      if ($user['email'] === $email) {
-        echo '<script>alert("Email is already in use")</script>';
-        $flag = $flag + 1;
-      }
+*/
 
     if($flag < 1){
-        
-       // $password = md5($password);//encrypt the password before saving
+        echo'<h1> Workers available on that date</h1>';
 
-        $sql = "INSERT INTO `customertb`(`id`, `username`, `user_password`, `email`, `user_address`, `user_level`) VALUES ('','$username','$password','$email','$address','')";
-        if(mysqli_query($success, $sql)){
-            
-        }else{
-            echo 'query error: ' . mysqli_error($success);
+
+        $result = $dbname->getData();
+        while($row = mysqli_fetch_assoc($result)){
+            if($row['appointment_date'] == 'date'){
+            workerslist($row['worker_id'], $row['worker_name'], $row['worker_email'], $row['service_type'], $row['worker_img'], $row['appointment_date'], $row['service_location'], $row['service_quality'], $row['price'], $row['worker_active']);  
         }
-
-        echo '<script>alert("Thank you for making an account")</script>';
-        echo "<script> location.href='login.php'; </script>";
-        exit;
-
-    }else{
-        echo "<script> location.href='signup.php'; </script>";
-        exit;
     }
 
 
-    ?>
+        exit;
+
+    }else{
+        echo "<script> location.href='date.php'; </script>";
+        exit;
+    }
--- date.php
+++ date.php
@@ -8,25 +8,18 @@
     <title>Check by date</title>
 </head>
 <body>
-    <div class="signup_form">
-        <form method="post" action="confirm_login.php">
-        <h1 class="title">Login</h1>
+    <h1>Input date to view</h1>
 
-        <div class="inputContainer">
-        <h1>Show a Date Control</h1>
-
-    <form action="check_date.php">
+    <form method="post" action="check_date.php">
         <label for="date">Check date:</label>
         <input type="date" id="date" name="date">
         <input type="submit">
     </form>
         </div>
-
             <p>
                 Need an account? <a href="check_date.php">Sign up</a></br>
             </p>
 
-        </form>
     </div>
 
 </br>
